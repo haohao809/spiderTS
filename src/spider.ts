6fef11b5dbb67f65cf48e7da7e4d320681c0516e
haohao809 <haohao809@126.com>
2020-02-20 16:12:39 +0800

    扒取百度肺炎疫情页面汇总数据

--- src/spider.ts
+++ src/spider.ts
@@ -1,15 +1,31 @@
 
 import superagent from 'superagent'
+import cheerio from 'cheerio'
 class Spider {
     private url = "https://voice.baidu.com/act/newpneumonia/newpneumonia/?from=osari_pc_1"
+    private data: string|undefined;
     constructor () {
         this.getHtml();
         console.log('constructor');
     }
     async getHtml() {
        const res = await superagent.get(this.url)
-       console.log(res.text);
+    //    console.log(res.text);
+       this.getHtmlContent(res.text);
     }
+    getHtmlContent(html :string) {
+        const $ = cheerio.load(html);
+        if($('script')[11].children[0].data) {
+            this.data = ($('script')[11].children[0].data);
+            if(this.data) {
+                const sumData = JSON.parse(this.data);
+                 if(sumData) {
+                     console.log(sumData.component[0].summaryDataIn);
+                 }
+             }
+        }
 
+        
+    }
 }
 const spider = new Spider();
